9d847f9fe5704d61e0f876fd9f56cd899fed89ea
Colin Baylis <colinbaylis@ucsb.edu>
2023-05-11 14:08:25 -0700

    CB - added Database Table

---
+++ src/main/java/edu/ucsb/cs156/example/repositories/StoresRepository.java
@@ -0,0 +1,12 @@
+package edu.ucsb.cs156.example.repositories;
+
+import edu.ucsb.cs156.example.entities.Store;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface StoresRepository extends CrudRepository<Store, Long> {
+  //Iterable<Store> findAllByQuarterYYYYQ(String quarterYYYYQ);
+}
