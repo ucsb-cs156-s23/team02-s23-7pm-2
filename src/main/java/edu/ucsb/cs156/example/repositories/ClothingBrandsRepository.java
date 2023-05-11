a3cc466b06c45c9a8bb56096f31602cfe3c8dec4
Nathan <nahoang@ucsb.edu>
2023-05-11 14:03:16 -0700

    Finished

--- src/main/java/edu/ucsb/cs156/example/repositories/ClothingBrandsRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/ClothingBrandsRepository.java
@@ -2,10 +2,11 @@ package edu.ucsb.cs156.example.repositories;
 
 import edu.ucsb.cs156.example.entities.ClothingBrands;
 
+import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ClothingBrandsRepository extends CrudRepository<ClothingBrands, Long> {
+public interface ClothingBrandsRepository extends CrudRepository<ClothingBrands, String> {
 }
